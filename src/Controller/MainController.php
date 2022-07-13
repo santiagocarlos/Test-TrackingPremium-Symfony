083f6ec62c5b11b9901bd716dbbb389b7d67da57
Carlos Santiago <sscarlos56@gmail.com>
2022-07-13 05:06:34 -0400

    Fisrt Commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MainController extends AbstractController
+{
+
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function getCompaniesTracking(): array
+    {
+        $url = 'https://apidemo.trackingpremium.us/publicapi/v1/search_username?username=trackingpremium';
+
+        $response = $this->client->request('GET', $url);
+        $parsedResponse = $response->toArray();
+
+        return $parsedResponse['maincompanies'];
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    #[Route('/', name: 'app_main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+            'companies' => $this->getCompaniesTracking(),
+        ]);
+    }
+
+
+}
